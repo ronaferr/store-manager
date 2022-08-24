37a03f461a7c3ed2a31c1eddd0f363104bdc2a63
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-08-23 23:10:21 -0300

    correção de cameCase em retorno de endpoint sales

--- models/salesModel.js
+++ models/salesModel.js
@@ -6,7 +6,13 @@ const getAll = async () => {
     INNER JOIN StoreManager.sales_products AS sp
     ON s.id = sp.sale_id;`;
   const [result] = await connection.execute(query);
-  return result;
+  const serialize = (authorData) => authorData.map((item) => ({
+    saleId: item.sale_id,
+    date: item.date,
+    productId: item.product_id,
+    quantity: item.quantity,
+  }));
+  return serialize(result);
 };
 
 const getById = async (id) => {
@@ -16,7 +22,12 @@ const getById = async (id) => {
     ON s.id = sp.sale_id
     WHERE s.id = ?;`;
   const [result] = await connection.execute(query, [id]);
-  return result;
+  const serialize = (authorData) => authorData.map((item) => ({
+    date: item.date,
+    productId: item.product_id,
+    quantity: item.quantity,
+  }));
+  return serialize(result);
 };
 
 /* const create = async ({ name }) => {
